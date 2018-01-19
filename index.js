be8611e3e9db274f72623f5c73f2ba7b21332d9a
stanciuionelamelania@gmail.com <stanciuionelamelania@gmail.com>
2018-01-19 22:39:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,18 +20,14 @@ function prependKitten(name){
 var kittens_copy = kittens_copy.slice()
 kittens_copy.unshift(name)
 return kittens_copy
-}  
+}
 function removeLastKitten() {
   var kittens_copy = kittens_copy.slice()
   kittens_copy.pop()
   return kittens_copy
 }
-  removes the last kitten in the kittens array and returns a new a
-rray, leaving the kittens array unchanged:
-     ReferenceError: removeLastKitten is not defined
-      at Context.<anonymous> (test/index-test.js:61:7)
-
-      8) Arrays removeFirstKitten() removes the first kitten from the kittens array and returns a n
-ew array, leaving the kittens array unchanged:
-     ReferenceError: removeFirstKitten is not defined
-      at Context.<anonymous> (test/index-test.js:69:7)
+function removeFirstKitten() {
+    var kittens_copy = kittens_copy.slice()
+    kittens_copy.shift()
+    return kittens_copy
+}
